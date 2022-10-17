eec1c85aa8a1a7851d4bcb27f61c29ffca33263d
amina-lyn <amina.layouni1@gmail.com>
2022-10-17 21:13:48 +0200

    page.product JS : OK

--- front/js/product.js
+++ front/js/product.js
@@ -1,1 +1,11 @@
- 
+ const pageUrl = document.location.href;
+ const id = (pageUrl.split `id=`[1]);
+ const baseUrl = "http://localhost:3000/api/";
+ const endPoint = 'products';
+ const url = baseUrl + endPoint + `/`+ id ;
+ fetch(url)
+.then(response => response.text())
+.then(text => {
+    const kanap = JSON.parse(text)
+    document.querySelector('#title').innerHTML=kanap.name;
+});
